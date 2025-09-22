1e301bba0d60d8162ce1ff447f9f55d3ef697645
ruka-gonza <regc750@gmail.com>
2025-09-21 21:22:12 -0300

    asd

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,4 +1,4 @@
-import state from '../state.js';
+import state from './state.js';
 
 const emoticonMap = {
     ':)': '🙂',
@@ -47,12 +47,9 @@ export function replaceEmoticons(text) {
 export function getUserIcons(user) {
     if (!user) return '';
 
-    // --- INICIO DE LA CORRECCIÓN CLAVE ---
-    // Si el usuario es un staff en modo incógnito (y yo soy staff para poder verlo)
     if (user.isActuallyStaffIncognito) {
         return `<span class="user-icon">👻</span>`;
     }
-    // --- FIN DE LA CORRECCIÓN CLAVE ---
 
     const roleIcons = {
         owner: '👑',
